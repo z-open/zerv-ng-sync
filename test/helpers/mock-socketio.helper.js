48f8109bc977a2f06bef52e4d43d9f885be60cb3
Emmanuel Telmon <etelmon@zimit.io>
2017-03-10 11:27:01 -0500

    documented mocksocketio

--- test/helpers/mock-socketio.helper.js
+++ test/helpers/mock-socketio.helper.js
@@ -6,12 +6,30 @@ function MockSocketio($q) {
 
     var self = this;
     this.network = true;
-    var events = {}, fetches = {};
+    var events = {}, 
+    fetches = {};
 
-    this.onFetch = function (operation, callback) {
+
+    this.onFetch = onFetch;
+    this.send = send;
+
+    this.on = on;
+    this.fetch = fetch;
+
+
+    /**
+     *  Register the call back that will be executed on the server side when fetch is called by the client
+     */
+    function onFetch(operation, callback) {
         fetches[operation] = callback;
     }
-    this.send = function (event, data, acknowledge) {
+
+    /** 
+     *  Send data thru the socket to the client from the server side
+     *  This will trigger the event callback on the client side
+     * 
+     */
+    function send(event, data, acknowledge) {
         var callback = events[event];
         if (callback) {
             return callback(data, acknowledge);
@@ -19,34 +37,31 @@ function MockSocketio($q) {
         return null;
     }
 
-    this.on = function (event, callback) {
+    /**
+     * the client registers to listen so specific event whose server will use to send data to.
+     */
+    function on(event, callback) {
         // if (!self.network) {
         //     return $q.defer().promise;
         // }
         console.log("ON: " + event);
         events[event] = callback;
-
-
-        // if (event === 'SYNC_NOW') {
-        //     self.backend.onPublicationNotficationCallback = callback;
-        // }
     }
-    this.fetch = function (operation, data) {
+
+    /**
+     *  The client uses fetch to send data over the server.
+     *  Server will react to the fetch via the callback registered with onFetch
+     */
+    function fetch(operation, data) {
         if (!self.network) {
             // never returns..
             return $q.defer().promise;
         }
         var fn = fetches[operation];
         if (fn) {
-             console.log('Fetching '+operation+' - ',data);
+            console.log('Fetching ' + operation + ' - ', data);
             return fn(data);
         }
-        // if (operation === 'sync.subscribe') {
-        //     return self.backend.subscribe(data);
-        // }
-        // if (operation === 'sync.unsubscribe') {
-        //     return self.backend.unsubscribe(data);
-        // }
     }
 }
 
